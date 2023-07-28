b0b7563bb32384ca12ab5f4fbacb28ec4e92a265
Aaronpteer <ocenaaronpeter2001@gmail.com>
2023-07-28 13:01:09 +0300

    changing

--- _printf.c
+++ _printf.c
@@ -2,52 +2,40 @@
 #include <unistd.h>
 #include "main.h"
 
-/**
- * _printf - Prints out the string format
- * @format: character string
- * Return: returns the number of chars
- */
-
 int _printf(const char *format, ...)
 {
+	unsigned int i = 0, str_c;
+	unsigned int totnum = 0;
 	va_list list;
-	unsigned int i = 0, totnum = 0;
-	char *stg;
 
-	if (!format)
+	if (!format || (format[i] == '%' && format[i + 1] == '\0'))
 		return (-1);
-
 	va_start(list, format);
 	while (format[i] != '\0')
 	{
 		if (format[i] != '%')
-		{ write(1, &format[i], 1), totnum++;
-		}
-		else
 		{
-			switch (format[i + 1])
-			{ case 'c':
-				i++;
-				{ char c = va_arg(list, int);
-					write(1, &c, 1), totnum++;
-				} break;
-			case 's':
-				stg = va_arg(list, char *);
-
-				if (stg == NULL)
-					stg = "(null)";
-				while (*stg != '\0')
-				{ write(1, stg, 1), stg++, totnum++;
-				} i++;
-				break;
-			case '%':
-				write(1, &format[i], 1), totnum++, i++;
-				break;
-			default:
-				write(1, &format[i], 1), totnum++;
-			}
-		} i++;
+			_puts(format[i]);
+		}
+		else if (format[i + 1] == 'c')
+		{
+			_puts(va_arg(list, int));
+			i++;
+		}
+		else if (format[i + 1] ==  's')
+		{
+			str_c = _gets(va_arg(list, char *));
+			i++;
+			totnum += (str_c - 1);
+		}
+		else if (format[i + 1] == '%')
+		{
+			_puts('%');
+		}
+		totnum++;
+		i++;
 	}
 	va_end(list);
 	return (totnum);
 }
+
